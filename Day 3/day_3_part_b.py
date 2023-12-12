d700d01468b697dee40d6b02ff7ed66c288c8bae
DidgeriDio <madcow747@gmail.com>
2023-12-12 20:15:59 +0900

    feat: day 3 part a finished
    Realised halfway through that a dictionary setup would be much better and then just index through that to find connecting ones but oh well. Would also be much better for part b so may pivot we'll see.

--- Day 3/day_3_part_b.py
+++ Day 3/day_3_part_b.py
@@ -1,43 +1,3 @@
-'''
-Initial plan for program
-
-Go through each line one at a time
-
-Run through the line checking for numbers. If you find a digit then check for digit until it
-returns false and then add each to a string then convert to int
-Add each number to a dictionary, the key is the number and the value is its positions + 1 either side. (Same as symbol)
-Set this dictionary to current number dict
-
-Cant use dictionaries as it doesnt accept multiple of the same number
-
-Go through each character in the line first checking for symbols. Put their position in a list
-that contains their position and the one lower and one above.
-Set this list to current symbol list
-
-
-Now you run through for the line and check what symbol positions overlap with the number positions in the dictionary
-If they do overlap you remove them from the list and add them to the total
-
-Moving to the next line you get the numbers and set current list (dict) to previous list
-and new numbers to current
-
-You then get the symbols and again set list to old list and new ones to current.
-
-Then you go through old numbers and check them versus new symbols.
-Again if they match remove from dict and add to total.
-
-Now you go through the current numbers checking them against the old symbols.
-If they match you remove them from the dictionary and add to the total
-
-Then you check current numbers versus current symbols
-Match = remove from dict and add to total
-
-Then you move to next line and repeat the process.
-
-You then check if new numbers are in contact with old symbols etc
-
-'''
-
 def set_number(number_string, number_position_list, current_numbers):
     if number_string != '':
         number = int(number_string)
@@ -94,39 +54,53 @@ with open("day_3.txt") as text:
                 number_string = ''
                 number_position_list = []
                 character_index += 1
+        print(f"Current numbers: {current_numbers}")
+        print(f"Current symbols: {current_symbols}")
+        print(f"Previous numbers: {previous_numbers}")
+        print(f"Previous symbols: {previous_symbols}")
+        print(f"Current numbers: {current_numbers}. Will check vs previous symbols")
         remove_list = []
         for inside_list in current_numbers:
             number = inside_list[0]
             for index in inside_list[1:]:
                 if index in previous_symbols:
+                    print(f"{number} found at {index} in previous_symbols so adding and removing it")
                     final_numbers.append(number)
                     remove_list.append(inside_list)
                     break
         if current_numbers != []:
             for item in remove_list:
                 current_numbers.remove(item)
+        print(f"New current numbers: {current_numbers}. Will now check vs current symbols")
         remove_list = []
         for inside_list in current_numbers:
             number = inside_list[0]
             for index in inside_list[1:]:
                 if index in current_symbols:
+                    print(f"{number} found at {index} in current_symbols so adding and removing it")
                     final_numbers.append(number)
                     remove_list.append(inside_list)
                     break
         if current_numbers != []:
             for item in remove_list:
                 current_numbers.remove(item)
+        print(f"New current numbers: {current_numbers}")
+        print(f"Previous numbers: {previous_numbers}")
         remove_list = []
         for inside_list in previous_numbers:
             number = inside_list[0]
             for index in inside_list[1:]:
                 if index in current_symbols:
+                    print(f"{number} found at {index} in current_symbols so adding and removing it")
                     final_numbers.append(number)
                     remove_list.append(inside_list)
                     break
         if previous_numbers != []:
             for item in remove_list:
                 previous_numbers.remove(item)
+        print(f"New previous numbers: {previous_numbers}")
+        print(final_numbers)
+    print(final_numbers)
     for number in final_numbers:
         total += number
     sum_numbers = sum(final_numbers)
