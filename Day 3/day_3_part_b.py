b871e6b4e332a8c0afe15c47e99f7369c64461e7
DidgeriDio <madcow747@gmail.com>
2023-12-12 22:17:55 +0900

    feat: finished day 3 part b
    Using the most ridiculous method I did manage it. Apologies for spaghetti code.

--- Day 3/day_3_part_b.py
+++ Day 3/day_3_part_b.py
@@ -1,3 +1,10 @@
+'''
+Could I loop through the gears instead and for each one check if one of the values(coordinates) match up with a number
+If they do add one to count and then run through again. If count is 2 at the end then multiply the numbers
+Will need to do future numbers too
+'''
+
+
 def set_number(number_string, number_position_list, current_numbers):
     if number_string != '':
         number = int(number_string)
@@ -13,20 +20,22 @@ with open("day_3.txt") as text:
     print(lines)
     current_numbers = []
     previous_numbers = []
+    twice_previous_numbers = []
     number_position_list = []
     current_symbols = []
-    previous_symbols = []
     number_string = ''
     final_numbers = []
     final_numbers_remove = []
     total = 0
+    line_count = 0
     for line in lines:
-        print(line)
+        line_count += 1
         line = f".{line}."
         print(line)
         character_index = 0
-        previous_symbols = current_symbols
+        previous_symbols = current_symbols.copy()
         current_symbols = []
+        twice_previous_numbers = previous_numbers
         previous_numbers = current_numbers
         current_numbers = []
         for character in line:
@@ -40,71 +49,68 @@ with open("day_3.txt") as text:
                 number_string += str(character)
                 number_position_list.append(character_index)
                 character_index += 1
-            else:
-                #character must be a symbol
-                if character_index != 0:
-                    minus_index = character_index - 1
-                else:
-                    minus_index = 0
-                plus_index = character_index + 1
-                current_symbols.append(minus_index)
+            elif character == "*":
+                #Gear
                 current_symbols.append(character_index)
-                current_symbols.append(plus_index)
                 set_number(number_string, number_position_list, current_numbers)
                 number_string = ''
                 number_position_list = []
                 character_index += 1
-        print(f"Current numbers: {current_numbers}")
-        print(f"Current symbols: {current_symbols}")
-        print(f"Previous numbers: {previous_numbers}")
-        print(f"Previous symbols: {previous_symbols}")
-        print(f"Current numbers: {current_numbers}. Will check vs previous symbols")
-        remove_list = []
-        for inside_list in current_numbers:
-            number = inside_list[0]
-            for index in inside_list[1:]:
-                if index in previous_symbols:
-                    print(f"{number} found at {index} in previous_symbols so adding and removing it")
-                    final_numbers.append(number)
-                    remove_list.append(inside_list)
-                    break
-        if current_numbers != []:
-            for item in remove_list:
-                current_numbers.remove(item)
-        print(f"New current numbers: {current_numbers}. Will now check vs current symbols")
-        remove_list = []
-        for inside_list in current_numbers:
-            number = inside_list[0]
-            for index in inside_list[1:]:
-                if index in current_symbols:
-                    print(f"{number} found at {index} in current_symbols so adding and removing it")
-                    final_numbers.append(number)
-                    remove_list.append(inside_list)
-                    break
-        if current_numbers != []:
-            for item in remove_list:
-                current_numbers.remove(item)
-        print(f"New current numbers: {current_numbers}")
-        print(f"Previous numbers: {previous_numbers}")
-        remove_list = []
-        for inside_list in previous_numbers:
-            number = inside_list[0]
-            for index in inside_list[1:]:
-                if index in current_symbols:
-                    print(f"{number} found at {index} in current_symbols so adding and removing it")
-                    final_numbers.append(number)
-                    remove_list.append(inside_list)
-                    break
-        if previous_numbers != []:
-            for item in remove_list:
-                previous_numbers.remove(item)
-        print(f"New previous numbers: {previous_numbers}")
-        print(final_numbers)
-    print(final_numbers)
-    for number in final_numbers:
-        total += number
-    sum_numbers = sum(final_numbers)
-    final_numbers = []
-
+            else:
+                character_index += 1
+        if line_count == 1:
+            print(f"Twice previous numbers: {twice_previous_numbers}")
+            print(f"Previous numbers: {previous_numbers}")
+            print(f"Current numbers: {current_numbers}")
+            print(f"Current symbols (what we use): {current_symbols}")
+            for gear in current_symbols:
+                gear_minus = gear -1
+                gear_plus = gear + 1
+                gears = [gear_minus, gear, gear_plus]
+                match_count = 0
+                number_list = []
+                for sublist in twice_previous_numbers:
+                    if any(gear in sublist[1:] for gear in gears):
+                        match_count += 1
+                        number_list.append(sublist[0])
+                for sublist in previous_numbers:
+                    if any(gear in sublist[1:] for gear in gears):
+                        match_count += 1
+                        number_list.append(sublist[0])
+                for sublist in current_numbers:
+                    if any(gear in sublist[1:] for gear in gears):
+                        match_count += 1
+                        number_list.append(sublist[0])
+                if match_count == 2:
+                    print(f"Found a match!")
+                    print(f"The two numbers are {number_list}")
+        else:
+            print(f"Twice previous numbers: {twice_previous_numbers}")
+            print(f"Previous numbers: {previous_numbers}")
+            print(f"Current numbers: {current_numbers}")
+            print(f"Previous symbols (what we use): {previous_symbols}")
+            for gear in previous_symbols:
+                print(gear)
+                gear_minus = gear - 1
+                gear_plus = gear + 1
+                gears = [gear_minus, gear, gear_plus]
+                match_count = 0
+                number_list = []
+                for sublist in twice_previous_numbers:
+                    if any(gear in sublist[1:] for gear in gears):
+                        match_count += 1
+                        number_list.append(sublist[0])
+                for sublist in previous_numbers:
+                    if any(gear in sublist[1:] for gear in gears):
+                        match_count += 1
+                        number_list.append(sublist[0])
+                for sublist in current_numbers:
+                    if any(gear in sublist[1:] for gear in gears):
+                        match_count += 1
+                        number_list.append(sublist[0])
+                if match_count == 2:
+                    print(f"Found a match!")
+                    print(f"The two numbers are {number_list}")
+                    sum = number_list[0] * number_list[1]
+                    total += sum
     print(f"Total:{total}")
-    print(f"Total summed:{sum_numbers}")
